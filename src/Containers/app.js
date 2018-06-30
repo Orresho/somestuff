e1205449e0be4597ce9aa9450168b8339515ae05
unknown <orhan.hajderovic@hotmail.com>
2018-06-30 08:04:22 +0200

    git commit

---
+++ src/Containers/app.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+// This will have all routes etc
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Hello world
+      </div>
+    );
+  }
+}
+
+export default App;
